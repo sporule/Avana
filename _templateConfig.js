59e67a281f8082b49bfa81c62f44b79361b7df09
Sporule <hao@sporule.com>
2019-11-01 21:27:06 +0000

    updated the posts to accept excludedtags

--- _templateConfig.js
+++ _templateConfig.js
@@ -2,7 +2,13 @@ const TemplateConfig = {
     github_link: "https://github.com/sporule/sporule",
     navs:
     {
-        "Home": "/"
+        "主页": "/",
+        "关于吃什么":"/",
+        "英伦四季高光":"/",
+        "英国吃什么":"/",
+        "英国玩什么":"/",
+        "英国很贵么":"/",
+        "英国之外呢":"/",
     },
     pageTypes:{
         "all":{
@@ -10,16 +16,44 @@ const TemplateConfig = {
             "subTitle":"SubTitle",
             "filterTags":[],
             "pageType":"all"
-        },
+        }
     },
     homeItems:[
         {
-            "title":"Title",
-            "titleBold":"BOLD",
-            "subTitle":"SubTitle",
+            "title":"英伦四季",
+            "titleBold":"高光",
+            "subTitle":"",
             "coverImage":"https://i.imgur.com/MrRLOC4.png",
-            "link":"/posts/?pagetype=uk-food",
+            "link":"/posts/categories/游记?tags=英国&pagetype=uk-food",
         },
+        {
+            "title":"英国",
+            "titleBold":"吃什么",
+            "subTitle":"",
+            "coverImage":"https://i.imgur.com/MrRLOC4.png",
+            "link":"/posts/categories/美食?tags=英国&pagetype=uk-food",
+        },
+        {
+            "title":"英国",
+            "titleBold":"玩什么",
+            "subTitle":"",
+            "coverImage":"https://i.imgur.com/MrRLOC4.png",
+            "link":"/posts/categories/攻略?tags=英国&pagetype=uk-food",
+        },
+        {
+            "title":"英国",
+            "titleBold":"很贵么",
+            "subTitle":"",
+            "coverImage":"https://i.imgur.com/MrRLOC4.png",
+            "link":"/posts/categories/预定?tags=英国&pagetype=uk-food",
+        },
+        {
+            "title":"英国",
+            "titleBold":"之外呢",
+            "subTitle":"",
+            "coverImage":"https://i.imgur.com/MrRLOC4.png",
+            "link":"/posts/categories/游记?extags=英国&pagetype=uk-food",
+        }
     ]
 }
 
