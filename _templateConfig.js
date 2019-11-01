db4f8fdfbeb54bfda32383c409be2d2884456cc5
Sporule <hao@sporule.com>
2019-11-01 19:47:18 +0000

    udpated default config

--- _templateConfig.js
+++ _templateConfig.js
@@ -7,32 +7,19 @@ const TemplateConfig = {
     pages: ["aboutUs"],
     pageTypes:{
         "all":{
-            "title":"所有文章",
-            "subTitle":"全部",
+            "title":"All Posts",
+            "subTitle":"SubTitle",
             "filterTags":[],
             "pageType":"all"
         },
-        "uk-food":{
-            "title":"英国美食",
-            "subTitle":"带你吃吃吃吃吃",
-            "filterTags":["美食","爱丁堡"],
-            "pageType":"uk-food"
-        }
     },
     homeItems:[
         {
-            "title":"英国",
-            "titleBold":"美食",
-            "subTitle":"吃吃吃吃",
-            "coverImage":"https://www.eatsomemore.com/images/IMG_20190414_021808_526.jpg",
-            "link":"/posts/categories/英国?pagetype=uk-food",
-        },
-        {
-            "title":"英国",
-            "titleBold":"美食",
-            "subTitle":"吃吃吃吃",
-            "coverImage":"https://www.eatsomemore.com/images/IMG_20190414_021808_526.jpg",
-            "link":"/posts/categories/英国?pagetype=uk-food",
+            "title":"Title",
+            "titleBold":"BOLD",
+            "subTitle":"SubTitle",
+            "coverImage":"https://i.imgur.com/MrRLOC4.png",
+            "link":"/posts/?pagetype=uk-food",
         },
     ]
 }
